aecaf8da4e256cd7e39d366f5ff0e0838be349e8
Mervegül Çetin <mervecetin0908@gmail.com>
2025-07-08 16:00:36 +0300

    authorization issues fixed

--- TV-Backend/Services/SanTsgTokenService.cs
+++ TV-Backend/Services/SanTsgTokenService.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TV_Backend.Models.Login;
 using TV_Backend.Services;
+
 public class SanTsgTokenService
 {
     private readonly IHttpClientFactory _httpClientFactory;
@@ -25,7 +26,6 @@ public class SanTsgTokenService
         if (_token != null && DateTime.Now < _expiresOn)
             return _token;
 
-        // Login bilgilerini configden al
         var loginRequest = new
         {
             Agency = _configuration["SanTsgApi:Agency"],
@@ -34,17 +34,16 @@ public class SanTsgTokenService
         };
 
         var client = _httpClientFactory.CreateClient();
-        var json = System.Text.Json.JsonSerializer.Serialize(loginRequest);
-        var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+        var json = JsonSerializer.Serialize(loginRequest);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
         var response = await client.PostAsync(_configuration["SanTsgApi:BaseUrl"] + "authenticationservice/login", content);
         response.EnsureSuccessStatusCode();
         var responseContent = await response.Content.ReadAsStringAsync();
 
-        // Response modelini kendi LoginResponse modeline göre deserialize et
-        var loginResponse = System.Text.Json.JsonSerializer.Deserialize<LoginResponse>(responseContent);
+        var loginResponse = JsonSerializer.Deserialize<LoginResponse>(responseContent);
 
         _token = "Bearer " + loginResponse.body.token;
-        _expiresOn = DateTime.Parse(loginResponse.body.expiresOn).AddMinutes(-5); // 5 dk erken expire et
+        _expiresOn = DateTime.Parse(loginResponse.body.expiresOn).AddMinutes(-5);
         return _token;
     }
 }
--- TV-Backend/obj/Debug/net8.0/TV-Backend.AssemblyInfo.cs
+++ TV-Backend/obj/Debug/net8.0/TV-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TV-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52836a1d9db0f59b423baa3e58f66c1d7a3d1942")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4c8fb7f67d6aa4d48c160ad054ed4d2ee37183")]
 [assembly: System.Reflection.AssemblyProductAttribute("TV-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TV-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
