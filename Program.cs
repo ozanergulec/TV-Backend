cd9439f188735ff0c9f65b02f41b9f7830595d14
Ozan Ergüleç <ozanergulec@hotmail.com>
2025-07-24 14:06:17 +0300

    fixed errors

--- Program.cs
+++
@@ -1,3 +0,0 @@
-using StackExchange.Redis;
-var redisConnectionString = builder.Configuration["Redis:ConnectionString"] ?? "localhost:6379";
-builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConnectionString)); 
